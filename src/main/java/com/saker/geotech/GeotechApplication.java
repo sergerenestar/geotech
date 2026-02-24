50e0c7de4ab1fcb11c74bf23760ace9c14097911
Serge Rene <serge.rene.anaba.eteme.vz@gmail.com>
2026-02-24 18:52:55 -0500

    Initial commit - Spring Boot project

---
+++ src/main/java/com/saker/geotech/GeotechApplication.java
@@ -0,0 +1,13 @@
+package com.saker.geotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saker/geotech/GeotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saker.geotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
